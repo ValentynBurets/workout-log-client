eedd4f3653f15e546eb33603c21ba3606936f35b
valentyn.b <valentyn.b@code-care.pro>
2023-03-11 12:22:56 +0200

    added body part component

--- src/components/BodyPart.tsx
+++ src/components/BodyPart.tsx
@@ -3,46 +3,61 @@ import { Stack, Typography } from "@mui/material";
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 
 import Icon from "../assets/icons/gym.png";
+import { ExerciseType } from "../types/ExerciseType";
 
 export interface IBodyPart {
-  item: any;
-  bodyPart: any;
-  setBodyPart: (arg: any) => void;
+  item: ExerciseType;
+  bodyPart: ExerciseType;
+  setBodyPart: (arg: ExerciseType) => void;
 }
 
 const BodyPart = (props: IBodyPart) => {
+  console.log(props);
   return (
     <Stack
-      // type="button"
+      //type="button"
       alignItems="center"
       justifyContent="center"
       className="bodyPart-card"
-      sx={{
-        borderTop: props.bodyPart === props.item ? "4px solid #ff2625" : "",
-        backgroundColor: "#fff",
-        borderBottomLeftRadius: "20px",
-        width: "270px",
-        height: "280px",
-        cursor: "pointer",
-        gap: "47px",
-      }}
-      onClick={()=> {
+      sx={
+        props.bodyPart === props.item
+          ? {
+              borderTop: "4px solid #FF2625",
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "47px",
+            }
+          : {
+              background: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "282px",
+              cursor: "pointer",
+              gap: "47px",
+            }
+      }
+      onClick={() => {
         props.setBodyPart(props.item);
-        window.scroll({top: 1800, left: 100, behavior: 'smooth'})
+        window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
       }}
     >
       <img
-        src={Icon}
+        src={props.item.gifUrl}
         alt="dumbbell"
-        style={{ width: "40px", height: "40px" }}
+        style={{ width: "90%", height: "80%" }}
       />
       <Typography
         fontSize="24px"
         fontWeight="bold"
+        fontFamily="Alegreya"
         color="#3A1212"
         textTransform="capitalize"
       >
-        {props.item}
+        {" "}
+        {props.item.name}
       </Typography>
     </Stack>
   );
